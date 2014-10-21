259aab52fd3f64d1e78161a941b5dc30376c5da2
Glenn <harris.glenn.jr@gmail.com>
2014-10-21 09:27:16 -0700

    added documentation to hash_from_two_arrays

--- hash_from_two_arrays.rb
+++ hash_from_two_arrays.rb
@@ -1,3 +1,8 @@
+# "Hash from two arrays" problem from http://rosettacode.org/wiki/Hash_from_two_arrays
+#######################################################################################
+#Using two Arrays of equal length, create a Hash object where the elements from one 
+# array (the keys) are linked to the elements of the other (the values) 
+
 test1,test2 = [1,2,3],[4,5,6]
 
 def construct_hash_from_two_arrays(first_array,second_array)
@@ -5,4 +10,4 @@ def construct_hash_from_two_arrays(first_array,second_array)
   Hash[*merged_arrays]
 end
 
-p construct_hash_from_two_arrays(test1,test2)
+construct_hash_from_two_arrays(test1,test2) #=>{1=>4, 2=>5, 3=>6}
