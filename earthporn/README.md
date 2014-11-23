958b0e9134183c12cce8627ad60801c990e5b7c4
Glenn <harris.glenn.jr@gmail.com>
2014-11-22 18:26:29 -0800

    can now download to file. added readme

---
+++ earthporn/README.md
@@ -0,0 +1,4 @@
+#EARTHPORN DOWNLOAD SCRIPT
+
+
+I wanted to automate downloading pictures to use as wallpaper on my laptop. Using the Reddit API I want to download the most recently uploaded photos from r/Earthporn to my local machine.
--- earthporn/get_pics.rb
+++ earthporn/get_pics.rb
@@ -7,16 +7,58 @@ class GetEarthPorn
   require 'rubygems'
   require 'httparty'
   require 'net/http'
+  require 'open-uri'
 
   include HTTParty
 
+  attr_reader :link, :picture_array
+
+  def initialize
+    @picture_array = []
+  end
+
   def get_pics
-    @link = get("http://www.reddit.com/r/EarthPorn/new/.json?limit=1")['data']['children'][0]["data"]["url"]
-    p @link
+    # picture array = []
+    # 10.times do 
+
+    # end
+    server_response = get("http://www.reddit.com/r/EarthPorn/new/.json?limit=2")['data']['children']
+    # p server_response.class
+    # p server_response.length
+    server_response.each do |image|
+      @picture_array << image["data"]["url"]
+    end
+
+    p @picture_array
+    p "*********************"
+    #['data']['children'][0]["data"]["url"] navigating down to link
+    # p @link
   end
 
   def save_to_disk
+    # open(@link) do |image|
+    #   p image.base_uri
+    #   p image.content_type
+    #   p image.charset
+    #   p image.content_encoding
+    #   p image.last_modified
+    # end
+    @picture_array.each do |image|
+      file = open(image)
+      file_name_to_save = image.split('/').last
 
+      open(file_name_to_save,'w') do |file|
+        open(image){|picture| file.write(picture.read)}
+      end      
+    end
+
+    # file = open(@link)
+    # p file
+    # file_name_to_save = @link.split('/').last
+
+    # open(file_name_to_save,'w') do |file|
+    #   open(@link){|image| file.write(image.read)}
+    # end
   end
 
 
@@ -37,5 +79,4 @@ e.get_pics
 e.save_to_disk
 
 
-
-
+###THIS WORKS FOR THE MOST PART, BUT NEED TO EXCLUDE THE FLICKR DOMAIN
