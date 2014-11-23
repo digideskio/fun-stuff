f504c8c937ba0411800210c33666ba5ab061fe4c
Glenn <harris.glenn.jr@gmail.com>
2014-11-22 18:42:11 -0800

    added conditional to exclude any links that don't include jpg or jpeg

--- earthporn/get_pics.rb
+++ earthporn/get_pics.rb
@@ -1,8 +1,3 @@
-#write code to get JSON from r/earthporn -- DONE!!!
-#determine what stream i want (new/popular?)
-##get the link info from each entry  -- HALFWAY DONE!!!
-#download and save the image from each link
-
 class GetEarthPorn
   require 'rubygems'
   require 'httparty'
@@ -11,38 +6,26 @@ class GetEarthPorn
 
   include HTTParty
 
-  attr_reader :link, :picture_array
+  attr_reader :picture_array
 
   def initialize
     @picture_array = []
   end
 
   def get_pics
-    # picture array = []
-    # 10.times do 
-
-    # end
-    server_response = get("http://www.reddit.com/r/EarthPorn/new/.json?limit=2")['data']['children']
-    # p server_response.class
-    # p server_response.length
+    server_response = get("http://www.reddit.com/r/EarthPorn/.json?limit=10")['data']['children']
     server_response.each do |image|
-      @picture_array << image["data"]["url"]
+      if image["data"]["url"].include?("jpg") || image["data"]["url"].include?("jpeg")
+        @picture_array << image["data"]["url"]
+      end
     end
 
     p @picture_array
     p "*********************"
-    #['data']['children'][0]["data"]["url"] navigating down to link
-    # p @link
+
   end
 
   def save_to_disk
-    # open(@link) do |image|
-    #   p image.base_uri
-    #   p image.content_type
-    #   p image.charset
-    #   p image.content_encoding
-    #   p image.last_modified
-    # end
     @picture_array.each do |image|
       file = open(image)
       file_name_to_save = image.split('/').last
@@ -51,14 +34,6 @@ class GetEarthPorn
         open(image){|picture| file.write(picture.read)}
       end      
     end
-
-    # file = open(@link)
-    # p file
-    # file_name_to_save = @link.split('/').last
-
-    # open(file_name_to_save,'w') do |file|
-    #   open(@link){|image| file.write(image.read)}
-    # end
   end
 
 
@@ -68,15 +43,4 @@ class GetEarthPorn
       raise WebserverError, response.code unless response.code == 200
       response
     end
-
-
-
-###HOW TO SAVE FILE FROM URL TO DISK???
 end
-
-e = GetEarthPorn.new
-e.get_pics
-e.save_to_disk
-
-
-###THIS WORKS FOR THE MOST PART, BUT NEED TO EXCLUDE THE FLICKR DOMAIN
