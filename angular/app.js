75eeb8325915dea6960b46402f20e3335a300453
Glenn <harris.glenn.jr@gmail.com>
2014-11-23 17:25:49 -0800

    stopped in the middle of panel controller video, 5:10

--- angular/app.js
+++ angular/app.js
@@ -8,15 +8,25 @@
 
   var gems = [{
       name: 'Dodecahedron',
-      price: 2.95,
+      price: 2.00,
       description: 'This is the bomb!',
-      canPurchase: true
+      canPurchase: false,
+      soldOut:false,
+      timePurchased: 1416782886046,
+      images:[{full:'ruby.jpg'},
+              {thumb:'ruby_thumb.jpg'}]
+
     },
     {
       name: "Pentagonal Gem",
-      price: 5.95,
+      price: 5.955,
       description: "It's a pentagon!",
-      canPurchase: true
+      canPurchase: false,
+      soldOut:false,
+      timePurchased: 1416782886046,
+      images:[{full:'green_gem.png'},
+              {thumb:'green_gem_thumb.jpg'}]
+
     }
   ];
 
