cd1a8891378d40af28b58cf847433ff0e8dd28ae
Glenn <harris.glenn.jr@gmail.com>
2014-12-07 19:34:13 -0800

    discovering angularjs --> One final feature I would like to showcase is Routing.

--- angular2/js/app.js
+++ angular2/js/app.js
@@ -1,6 +1,17 @@
 (function(){
 
-  var app = angular.module("myCountersApp",['ngRoute'])
+  var app = angular.module("myApp",['ngResource'])
+  app.controller('myController', ['$scope','$resource',function($scope,$resource){
+    var counterResource = $resource("api/counters/index.json");
+
+    counterResource.query(function(response){
+      response[2].id=3;
+      console.log(response)
+      // response[1].name="third";
+      // response.$save();
+      console.log(response);
+    })
+  }])
 
 
 
