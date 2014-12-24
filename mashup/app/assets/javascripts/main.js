5d401e235dc64395503d422781fdd0aa0d213596
Glenn <harris.glenn.jr@gmail.com>
2014-12-23 20:29:09 -0800

    Post model serves JSON properly. Angular routes working properly. Added some directives to try stuff out. Get request on Index is working great. bad styling though...

--- mashup/app/assets/javascripts/main.js
+++ mashup/app/assets/javascripts/main.js
@@ -1,9 +1,20 @@
-angular.module('mashup',[
+app = angular.module('mashup',[
   "templates",
   "ngRoute"
   ])
-  .controller("MashupController",["$scope","$routeParams","$location", 
-    function($scope,$routeParams,$location){
-
-      $scope.glenn = "hello babay!"
+  .config(["$routeProvider","$locationProvider", function($routeProvider,$locationProvider){
+    $routeProvider
+      .when('/',{
+        templateUrl:"index.html",
+        controller:"MashupController"
+      });
+  }])
+  .controller("MashupController",["$scope","$http", "$routeParams","$location", 
+    function($scope,$http,$routeParams,$location){
+      //declare $scope.posts to be Post.all array on 200 Ok response
+      $http.get('../posts')
+                .success(function(response){
+                  $scope.posts = response
+                  console.log($scope.posts)
+                })
     }])
