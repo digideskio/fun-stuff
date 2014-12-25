8cad8be423459a4617c049bddf64f1e8c303ae21
Glenn <harris.glenn.jr@gmail.com>
2014-12-24 23:23:54 -0800

    created routes to serve JSON. Able to pull JSON into angular controller. Need to write function to return 'n' number of quotes where n is the number of paragraphs requested by the user.

--- morpheusrails/app/controllers/quotes_controller.rb
+++ morpheusrails/app/controllers/quotes_controller.rb
@@ -1,4 +1,8 @@
 class QuotesController < ApplicationController
   def index
+    @quotes = Quote.all.shuffle
+    respond_to do |format|
+      format.json {render json:@quotes}
+    end
   end
 end
